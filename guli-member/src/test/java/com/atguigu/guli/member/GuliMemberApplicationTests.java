6e04d1c8c2326c175528b9d5d21f1d42039bc732
zenghanghang <359022302@qq.com>
2023-12-29 17:18:22 +0800

    project initialize

---
+++ guli-member/src/test/java/com/atguigu/guli/member/GuliMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.guli.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
