209a5fb772def9e5e6cded00ed6be42498637de2
zenghanghang <359022302@qq.com>
2024-01-05 11:10:31 +0800

    add gateway

---
+++ gulimall-gateway3/src/test/java/com/atguigu/gulimall/gateway3/GulimallGateway3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.gateway3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallGateway3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
