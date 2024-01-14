0cda56307fb2942a78423ad31f09a985c6921f70
zenghanghang <359022302@qq.com>
2024-01-14 15:00:32 +0800

    开发product模块的参数检验

--- guli-product/src/main/java/com/atguigu/gulimall/guliproduct/controller/BrandController.java
+++ guli-product/src/main/java/com/atguigu/gulimall/guliproduct/controller/BrandController.java
@@ -1,9 +1,20 @@
 package com.atguigu.gulimall.guliproduct.controller;
 
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.OSSException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +26,7 @@ import com.atguigu.gulimall.guliproduct.service.BrandService;
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.R;
 
+import javax.annotation.Resource;
 
 
 /**
@@ -29,15 +41,64 @@ import com.atguigu.common.utils.R;
 public class BrandController {
     @Autowired
     private BrandService brandService;
+    @Autowired
+    OSS oSSClient;
 
     /**
      * 列表
      */
     @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = brandService.queryPage(params);
 
         return R.ok().put("page", page);
+    }
+
+    @RequestMapping("/testUpload")
+    public void testUpload() {
+        {
+//        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+//        String endpoint = "https://oss-cn-hangzhou.aliyuncs.com";
+//        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+//        String accessKeyId = "yourAccessKeyId";
+//        String accessKeySecret = "yourAccessKeySecret";
+            // 填写Bucket名称，例如examplebucket。
+            String bucketName = "gulimall-zhhproduct";
+            // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+            String objectName = "testPic.png";
+            // 填写本地文件的完整路径，例如D:\\localpath\\examplefile.txt。
+            // 如果未指定本地路径，则默认从示例程序所属项目对应本地路径中上传文件流。
+            String filePath = "/Users/a8000_1/zenghanghang/personal_env/guli-shop/guli/guli-product/testPic.png";
+
+//        // 创建OSSClient实例。
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+            try {
+                InputStream inputStream = new FileInputStream(filePath);
+                // 创建PutObject请求。
+                oSSClient.putObject(bucketName, objectName, inputStream);
+            } catch (OSSException oe) {
+                System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                        + "but was rejected with an error response for some reason.");
+                System.out.println("Error Message:" + oe.getErrorMessage());
+                System.out.println("Error Code:" + oe.getErrorCode());
+                System.out.println("Request ID:" + oe.getRequestId());
+                System.out.println("Host ID:" + oe.getHostId());
+            } catch (ClientException ce) {
+                System.out.println("Caught an ClientException, which means the client encountered "
+                        + "a serious internal problem while trying to communicate with OSS, "
+                        + "such as not being able to access the network.");
+                System.out.println("Error Message:" + ce.getMessage());
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                if (oSSClient != null) {
+                    oSSClient.shutdown();
+                }
+            }
+            System.out.println("上传成功");
+        }
+
     }
 
 
@@ -45,8 +106,8 @@ public class BrandController {
      * 信息
      */
     @RequestMapping("/info/{brandId}")
-    public R info(@PathVariable("brandId") Long brandId){
-		BrandEntity brand = brandService.getById(brandId);
+    public R info(@PathVariable("brandId") Long brandId) {
+        BrandEntity brand = brandService.getById(brandId);
 
         return R.ok().put("brand", brand);
     }
@@ -54,19 +115,28 @@ public class BrandController {
     /**
      * 保存
      */
-    @RequestMapping("/save")
-    public R save(@RequestBody BrandEntity brand){
-		brandService.save(brand);
 
+    @RequestMapping("/save")
+    public R save(@Validated @RequestBody BrandEntity brand, BindingResult result) {
+        HashMap<String, String> map = new HashMap<>();
+        if (result.hasErrors()) {
+            result.getFieldErrors().stream()
+                    .forEach((ele) -> {
+                        map.put(ele.getField(), ele.getDefaultMessage());
+                    });
+            return R.error(400, "数据不符合规范").put("data", map);
+        }
+        brandService.save(brand);
         return R.ok();
+
     }
 
     /**
      * 修改
      */
     @RequestMapping("/update")
-    public R update(@RequestBody BrandEntity brand){
-		brandService.updateById(brand);
+    public R update(@RequestBody BrandEntity brand) {
+        brandService.updateById(brand);
 
         return R.ok();
     }
@@ -75,8 +145,8 @@ public class BrandController {
      * 删除
      */
     @RequestMapping("/delete")
-    public R delete(@RequestBody Long[] brandIds){
-		brandService.removeByIds(Arrays.asList(brandIds));
+    public R delete(@RequestBody Long[] brandIds) {
+        brandService.removeByIds(Arrays.asList(brandIds));
 
         return R.ok();
     }
--- guli-product/src/main/java/com/atguigu/gulimall/guliproduct/entity/BrandEntity.java
+++ guli-product/src/main/java/com/atguigu/gulimall/guliproduct/entity/BrandEntity.java
@@ -6,6 +6,12 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 /**
  * 品牌
@@ -27,10 +33,13 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 品牌名
 	 */
+	@NotBlank(message = "品牌名称不为空")
 	private String name;
 	/**
 	 * 品牌logo地址
 	 */
+	@URL
+	@NotBlank(message = "LOGO URL不为空")
 	private String logo;
 	/**
 	 * 介绍
@@ -39,14 +48,18 @@ public class BrandEntity implements Serializable {
 	/**
 	 * 显示状态[0-不显示；1-显示]
 	 */
+	@NotNull(message = "展示状态不能为空")
 	private Integer showStatus;
 	/**
 	 * 检索首字母
 	 */
+	@Pattern(regexp = "^[a-zA-Z]$",message = "检索首字母必须是一个字母")
 	private String firstLetter;
 	/**
 	 * 排序
 	 */
+	@NotNull
+	@Min(value = 0,message = "排序必须大于等于0")
 	private Integer sort;
 
 }
